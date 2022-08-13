757fb38686ffd558d66cfe156ad9b990510ee4b6
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-12 21:48:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ import pandas as pd
 import streamlit as st
 
 
-with st.echo():
-    st.write('This code will be printed')
+with st.write('This code will be printed'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
