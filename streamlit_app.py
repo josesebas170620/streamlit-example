6083fc01303d026421f90ab98cec7cf7de0358bd
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-12 21:19:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         header {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
