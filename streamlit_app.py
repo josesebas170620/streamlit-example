1f9a0fc91571c8320847ca5d367098be63724199
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-12 21:31:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,12 @@ with st.echo(code_location='below'):
         radius = curr_point_num / total_points
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
 
-    
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
     
 
 hide_menu_style = """
