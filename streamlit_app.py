0998759f904205d398a67db08e8baa10799773e5
josesebas170620 <111162408+josesebas170620@users.noreply.github.com>
2022-08-12 21:31:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ with st.echo(code_location='below'):
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
 
     
 
